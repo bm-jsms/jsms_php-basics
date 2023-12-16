d9bd660e7773fc27aad961e726b7587687ab56dc
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-12-16 16:33:58 -0500

    example 1

---
+++ test_1.php
@@ -0,0 +1,26 @@
+<?php
+
+//string
+$name = "Josué";
+
+//integer
+$age = 18;
+
+//double
+$height = 1.75;
+
+//boolean
+$married = false;
+
+//array
+$week = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+
+//object
+$car = (object) array('brand' => 'Ford', 'model' => 'Fiesta', 'color' => 'blue');
+
+//null
+$null = null;
+
+echo "My name is $name, I'm $age years old, I'm $height meters tall, the third day of the week is \"$week[2]\"";
+
+?>
