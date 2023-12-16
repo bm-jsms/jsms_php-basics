d5277c449ff73b61d992afcf8a6469882ec3cec3
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-12-16 16:43:58 -0500

    example 2

---
+++ test-2.php
@@ -0,0 +1,31 @@
+<?php
+
+$variable = 5;
+//sum
+$variable += 5;
+
+//subtraction
+$variable -= 5;
+
+//multiplication
+$variable *= 5;
+
+//division
+$variable /= 5;
+
+//modulus
+$variable %= 5;
+
+echo $variable; // 0
+
+$a = 5;
+$b = 10;
+
+$a += $b; // $a = $a + $b;
+
+// concatenation
+$text = "Hello";
+$variable .= " " . "World";
+$variable .= " World";
+
+?>
