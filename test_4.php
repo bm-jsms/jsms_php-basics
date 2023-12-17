909a22d6c21edebd344164208b40485f1ba22a4b
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-12-17 14:40:42 -0500

    example 4 - array methods

--- test_4.php
+++ test_4.php
@@ -19,4 +19,25 @@ $people = array(
 );
 echo $people['online'][0]; // "David"
 
+
+echo count($people); // 3
+echo "<br>";
+echo count($people['online']); // 2
+
+
+array_push($people['online'], 'Javier'); // Add element at the end of the array
+
+
+is_array($people); // true
+
+
+//explode
+$fruit = "apple,banana,orange";
+
+$fruitArray = explode(",", $fruit); // Array ( [0] => apple [1] => banana [2] => orange )
+
+//implode
+$fruitString = implode(",", $fruitArray); // apple,banana,orange
+
+
 ?> 
