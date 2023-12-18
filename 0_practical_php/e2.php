eafe61126f480bbc0f9c7479eb543852cedb35b9
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-12-18 16:11:20 -0500

    e2

---
+++ 0_practical_php/e2.php
@@ -0,0 +1,25 @@
+<?php 
+
+$number1 = 55;
+$number2 = 33;
+
+
+$result = $number1 + $number2;
+var_dump($result); // 88
+
+$result = $number1 - $number2;
+var_dump($result); // 22
+
+$result = $number1 * $number2;
+var_dump($result); // 1815
+
+$result = $number1 / $number2;
+var_dump($result); // 1.6666666666667
+
+$result = $number1 % $number2;
+var_dump($result); // 22
+
+$result = $number1 ** $number2;
+var_dump($result); // 1.0660181603285E+47
+
+?>
