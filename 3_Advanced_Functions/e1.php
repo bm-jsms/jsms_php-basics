4294bc78fa673b5deeec5583f72b36c292822ba4
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-12-30 17:10:52 -0500

    Advanced Functions - e1

---
+++ 3_Advanced_Functions/e1.php
@@ -0,0 +1,13 @@
+<?php
+
+# define('PI', 3.14159, true);  // true is for case-insensitive  
+# define('pi', 3.14159);        // false is default, case-sensitive
+# echo PI; // 3.14159
+# echo pi; // 3.14159
+
+const PI = 3.14159;
+const pi = 3.14159; 
+echo PI; // 3.14159
+echo pi; // 3.14159
+
+?>
