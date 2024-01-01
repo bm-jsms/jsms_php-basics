5ffb67ff8b9af2f23f45bba93b0b77093b317578
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-01 01:22:21 -0500

    Exception - e2

---
+++ 6_exception/e2.php
@@ -0,0 +1,15 @@
+<?php
+
+// i like dogs
+
+try {
+    $pet = readline("Enter your pet's name: ");
+
+    if ($pet != "dog") {
+        throw new Exception("You don't have a dog!");
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+?>
