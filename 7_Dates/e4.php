3bdf85af40a9c06555aae555b989a36d40b11510
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-03 01:35:08 -0500

    Dates - e4 - r

--- 7_Dates/e4.php
+++ 7_Dates/e4.php
@@ -4,4 +4,6 @@ $today = new DateTime();
 $neighbour_birthday = new DateTime('2000-05-05');
 $interval = $neighbour_birthday->diff($today);
 
+echo $interval->format('%y years, %m months, %d days');
+
 ?>
